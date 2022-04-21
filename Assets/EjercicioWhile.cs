eb22f48c4096954afa8484b45ed5269bbfcc21d6
Marco Canton <67104790+MarcoCanton1@users.noreply.github.com>
2022-04-21 15:56:48 -0300

    Avances

---
+++ Assets/EjercicioWhile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjercicioWhile : MonoBehaviour
+{
+    public int numero1;
+    public int numero2;
+    void Start()
+    {
+        if (numero1 != numero2 && numero1 > numero2)
+        {
+            while (numero2 <= numero1)
+            {
+                Debug.Log(numero1);
+                numero1--;
+            }
+        }
+        else
+        {
+            Debug.Log("Los numeros son iguales o el primer numero es mayor al segundo");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/While01.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class While01 : MonoBehaviour
+{
+    int counter = 1;
+    void Start()
+    {
+        while (counter < 100)
+        {
+            counter++;
+            Debug.Log(counter);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
